7a8497ae2f11b2d33b7c6bf1272d8fae78e9e750
DmitryChernavtsev <74550443+DmitryChernavtsev@users.noreply.github.com>
2021-02-04 17:56:15 +0300

    Add files via upload

---
+++ poisk.py
@@ -0,0 +1,21 @@
+import sys
+
+import requests
+from functions import geocode, get_coordinates, get_ll_span, show_map
+
+def main():
+    toponym_to_find = " ".join(sys.argv[1:])
+
+    if toponym_to_find:
+
+        ll, spn = get_ll_span(toponym_to_find)
+        ll_spn = f"ll={ll}&spn={spn}"
+
+        point_param = f"pt={ll}"
+        show_map(ll_spn, "map", add_params=point_param)
+    else:
+        print('No data')
+
+
+if __name__ == '__main__':
+    main()
